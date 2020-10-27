defa2b05cd94a8dbe8d79c6a88123819b822a33d
keigo.yago <keigoya1995@gmail.com>
2020-10-27 01:03:04 +0000

    add column

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "新規タスクが投稿されました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "新規タスクを投稿できませんでした"
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+
+    if @task.update(task_params)
+      flash[:success] = "タスクは更新されました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクは更新されませんでした"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task.destroy
+    
+    flash[:success] = "タスクは削除されました"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+end
